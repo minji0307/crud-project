91620871eb348819e3a2fbb5d412cbcbffae6072
kimminji <hpfuture0307@likelion.org>
2019-12-30 21:35:00 +0900

    first commit of crud project

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
+import Login from './components/login';
+import Books from './components/books';
 
 function App() {
+
+  const [token, setToken] = useState('');
+
+  const userLogin = (tok) => {
+    setToken(tok);
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Login userLogin={userLogin}/>
+      <Books token={token}/>
     </div>
   );
 }
